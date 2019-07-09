c6db72e97d07c754cddf86eb440d79f038cf0348
mazdak <nazemi.works@gmail.com>
2019-07-08 22:56:31 -0700

    home page style of items done

--- sd/src/components/MenuItem/MenuItem.js
+++ sd/src/components/MenuItem/MenuItem.js
@@ -4,9 +4,10 @@ class MenuItem extends Component {
   render() {
     const { title, imageUrl, size } = this.props;
     return (
-      <div className={`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
+      <div className={`${size} menu-item`}>
+        <div className='background-image'  style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className='content'>
-          <h1 className='title'>{title}</h1>
+          <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>shop now</span>
         </div>
       </div>
