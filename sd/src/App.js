cddb29457e1352effa694d2b2b02780ddbdc271b
mazdak <nazemi.works@gmail.com>
2019-07-09 06:25:33 -0700

    make use of unsubscribe from g in unmount

--- sd/src/App.js
+++ sd/src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch ,Route } from 'react-router-dom';
-
 import './App.css';
 
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import Shop from './pages/ShopPage/Shop';
 import SignPage from './pages/SignPage/SignPage';
+// now after firebase work in console we want to let app know
+// to store user to state of app
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
+
+
+class App extends Component {
+  state = {
+    currentUser: null,
+  }
+  
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    // this is open subscription this connection is always open
+    // but we must close it on unmounted
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      // console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+    return (
+      <div>
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
@@ -18,7 +41,10 @@ function App() {
         <Route exact path='/signin' component={SignPage}/>
       </Switch>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+
