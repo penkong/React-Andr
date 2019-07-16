755879e7b24152e0aa9c562704df53b91b83b08f
mazdak <nazemi.works@gmail.com>
2019-07-16 02:35:57 -0700

    refactor with useState and hooks

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {  useState } from 'react';
 import { connect } from 'react-redux'
 
 import './SignInStyles.scss';
@@ -8,66 +8,106 @@ import CustButton from '../CustButton/CustButton';
 // import { auth } from '../../firebase/firebase.utils';
 import { googleSingInStart, emailSingInStart } from '../../redux/user/userAction';
 
-
-class SignIn extends Component {
-  state = { email: '', password: '' };
-
-  handleSubmit = async e => {
-    const { email, password } = this.state;
-    const { emailSingInStart } = this.props;
+const SignIn = ({ emailSingInStart, googleSingInStart }) => {
+  const [ userCredentials, setUserCredentials ] = useState({ email: '', password: ''});
+  const { email, password } = userCredentials;
+  const handleSubmit = async e => {
     e.preventDefault();
     emailSingInStart(email, password);
-    // try {
-    //   await auth.signInWithEmailAndPassword(email, password);
-    //   this.setState({email:'', password: ''});
-    // } catch (error) {
-    //   console.log(error);
-    // }
   }
-
-  handleChange = e => {
+  const handleChange = e => {
     const { value, name } = e.target;
-    this.setState({ [name] : value });
+    setUserCredentials({ ...userCredentials, [name] : value });
   }
-
-  render() {
-    const { googleSingInStart } = this.props;
-    return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sing in with your email and password.</span>
-        <form onSubmit={this.handleSubmit}>
-          <FormInput 
-            required 
-            name='email' 
-            type='email' 
-            label='email'
-            value={this.state.email} 
-            handleChange={this.handleChange}
-            />
-          <FormInput 
-            required 
-            label='password'
-            name='password' 
-            type='password' 
-            value={this.state.password} 
-            handleChange={this.handleChange}
+  return (
+    <div className='sign-in'>
+      <h2>I already have an account</h2>
+      <span>Sing in with your email and password.</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput 
+          required 
+          name='email' 
+          type='email' 
+          label='email'
+          value={email} 
+          handleChange={handleChange}
           />
-          <div className='buttons'>
-            <CustButton type="submit"> Sign in </CustButton>
-            <CustButton type="button" Click={googleSingInStart} isGoogleSignIn> 
-              SIGN IN WITH GOOGLE 
-            </CustButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        <FormInput 
+          required 
+          label='password'
+          name='password' 
+          type='password' 
+          value={password} 
+          handleChange={handleChange}
+        />
+        <div className='buttons'>
+          <CustButton type="submit"> Sign in </CustButton>
+          <CustButton type="button" Click={googleSingInStart} isGoogleSignIn> 
+            SIGN IN WITH GOOGLE 
+          </CustButton>
+        </div>
+      </form>
+    </div>
+  )
 }
 const mapDispatchToProps = dispatch => ({
   googleSingInStart: () => dispatch(googleSingInStart()),
   emailSingInStart: (email, password) => 
     dispatch(emailSingInStart({ email, password })),
 })
-
 export default connect(null, mapDispatchToProps)(SignIn);
+// class SignIn extends Component {
+//   state = { email: '', password: '' };
+
+//   handleSubmit = async e => {
+//     const { email, password } = this.state;
+//     const { emailSingInStart } = this.props;
+//     e.preventDefault();
+//     emailSingInStart(email, password);
+//     // try {
+//     //   await auth.signInWithEmailAndPassword(email, password);
+//     //   this.setState({email:'', password: ''});
+//     // } catch (error) {
+//     //   console.log(error);
+//     // }
+//   }
+
+//   handleChange = e => {
+//     const { value, name } = e.target;
+//     this.setState({ [name] : value });
+//   }
+
+//   render() {
+//     const { googleSingInStart } = this.props;
+//     return (
+//       <div className='sign-in'>
+//         <h2>I already have an account</h2>
+//         <span>Sing in with your email and password.</span>
+//         <form onSubmit={this.handleSubmit}>
+//           <FormInput 
+//             required 
+//             name='email' 
+//             type='email' 
+//             label='email'
+//             value={this.state.email} 
+//             handleChange={this.handleChange}
+//             />
+//           <FormInput 
+//             required 
+//             label='password'
+//             name='password' 
+//             type='password' 
+//             value={this.state.password} 
+//             handleChange={this.handleChange}
+//           />
+//           <div className='buttons'>
+//             <CustButton type="submit"> Sign in </CustButton>
+//             <CustButton type="button" Click={googleSingInStart} isGoogleSignIn> 
+//               SIGN IN WITH GOOGLE 
+//             </CustButton>
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 
 import './SignUpStyles.scss';
@@ -7,92 +7,159 @@ import CustButton from '../CustButton/CustButton';
 // import { auth , createUserProfileDocument } from '../../firebase/firebase.utils';
 import { signUpStart } from "../../redux/user/userAction";
 
-class SignUp extends Component {
-  state = { 
+
+const SignUp = ({ signUpStart }) => {
+  const [userCredentials, setUserCredentials] = useState({ 
     displayName : '', 
     email: '', 
     password: '', 
     confirmPassword: ''
-  };
-
-  handleSubmit = async e => {
-    const { displayName, email, password, confirmPassword } = this.state;
-    const { signUpStart } = this.props;
+  });
+  const { displayName, email, password, confirmPassword } = userCredentials;
+  const handleSubmit = async e => {
     e.preventDefault();
-
     if(password !== confirmPassword) {
       alert("Password match wrong")
       return;
     }
-
     signUpStart({ displayName, email, password });
-    // try {
-    //   const { user } = await auth.createUserWithEmailAndPassword(email, password);
-    //   await createUserProfileDocument(user, {displayName});
-    //   this.setState({ 
-    //     displayName : '', 
-    //     email: '', 
-    //     password: '', 
-    //     confirmPassword: ''
-    //   });
-    // } catch (error) {
-    //   console.log(error);
-    // }
   }
 
-  handleChange = e => {
+  const handleChange = e => {
     const { value, name } = e.target;
-    this.setState({ [name] : value });
+    setUserCredentials({ ...userCredentials, [name] : value });
   }
-
-  render() {
-    const { displayName, email, password, confirmPassword } = this.state;
-    return (
-      <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
-        <span>Sign up with your email and password</span>
-        <form className='sign-up-form' onSubmit={this.handleSubmit}>
-          <FormInput 
-            required
-            type='text'
-            name='displayName'
-            label='Display Name'
-            value={displayName}
-            onChange={this.handleChange}
-          />
-          <FormInput 
-            required
-            type='email'
-            name='email'
-            label='Email'
-            value={email}
-            onChange={this.handleChange}
-          />
-          <FormInput 
-            required
-            type='password'
-            name='password'
-            label='Password'
-            value={password}
-            onChange={this.handleChange}
-          />
-          <FormInput 
-            required
-            type='password'
-            name='confirmPassword'
-            label='Confirm Password'
-            value={confirmPassword}
-            onChange={this.handleChange}
-          />
-
-          <CustButton type='submit'> Sign Up </CustButton>
-
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className='sign-up'>
+    <h2 className='title'>I do not have an account</h2>
+    <span>Sign up with your email and password</span>
+    <form className='sign-up-form' onSubmit={handleSubmit}>
+      <FormInput 
+        required
+        type='text'
+        name='displayName'
+        label='Display Name'
+        value={displayName}
+        onChange={handleChange}
+      />
+      <FormInput 
+        required
+        type='email'
+        name='email'
+        label='Email'
+        value={email}
+        onChange={handleChange}
+      />
+      <FormInput 
+        required
+        type='password'
+        name='password'
+        label='Password'
+        value={password}
+        onChange={handleChange}
+      />
+      <FormInput 
+        required
+        type='password'
+        name='confirmPassword'
+        label='Confirm Password'
+        value={confirmPassword}
+        onChange={handleChange}
+      />
+      <CustButton type='submit'> Sign Up </CustButton>
+    </form>
+  </div>
+  )
 }
+
 const mapDispatchToProps = dispatch => ({
   signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
 });
+
 export default connect(null, mapDispatchToProps)(SignUp);
+
+// class SignUp extends Component {
+//   state = { 
+//     displayName : '', 
+//     email: '', 
+//     password: '', 
+//     confirmPassword: ''
+//   };
+
+//   handleSubmit = async e => {
+//     const { displayName, email, password, confirmPassword } = this.state;
+//     const { signUpStart } = this.props;
+//     e.preventDefault();
+
+//     if(password !== confirmPassword) {
+//       alert("Password match wrong")
+//       return;
+//     }
+
+//     signUpStart({ displayName, email, password });
+//     // try {
+//     //   const { user } = await auth.createUserWithEmailAndPassword(email, password);
+//     //   await createUserProfileDocument(user, {displayName});
+//     //   this.setState({ 
+//     //     displayName : '', 
+//     //     email: '', 
+//     //     password: '', 
+//     //     confirmPassword: ''
+//     //   });
+//     // } catch (error) {
+//     //   console.log(error);
+//     // }
+//   }
+
+//   handleChange = e => {
+//     const { value, name } = e.target;
+//     this.setState({ [name] : value });
+//   }
+
+//   render() {
+//     const { displayName, email, password, confirmPassword } = this.state;
+//     return (
+//       <div className='sign-up'>
+//         <h2 className='title'>I do not have an account</h2>
+//         <span>Sign up with your email and password</span>
+//         <form className='sign-up-form' onSubmit={this.handleSubmit}>
+//           <FormInput 
+//             required
+//             type='text'
+//             name='displayName'
+//             label='Display Name'
+//             value={displayName}
+//             onChange={this.handleChange}
+//           />
+//           <FormInput 
+//             required
+//             type='email'
+//             name='email'
+//             label='Email'
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//           <FormInput 
+//             required
+//             type='password'
+//             name='password'
+//             label='Password'
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//           <FormInput 
+//             required
+//             type='password'
+//             name='confirmPassword'
+//             label='Confirm Password'
+//             value={confirmPassword}
+//             onChange={this.handleChange}
+//           />
+
+//           <CustButton type='submit'> Sign Up </CustButton>
+
+//         </form>
+//       </div>
+//     );
+//   }
+// }
