7b2c50372067bdd27ae85c2ffb91bfff74bfe92f
mazdak <nazemi.works@gmail.com>
2019-07-17 05:34:56 -0700

    add new client for context learning

---
+++ client2/react-context-lesson/src/pages/collection/collection.component.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+// import { connect } from 'react-redux';
+import CollectionsContext from '../../context/collections/collectionsContext';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+// import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+// const mapStateToProps = (state, ownProps) => ({
+//   collection: selectCollection(ownProps.match.params.collectionId)(state)
+// });
+
+export default CollectionPage;
