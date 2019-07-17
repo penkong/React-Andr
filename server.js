117a2d98911694f944a271cb02c8a8ec23387c48
mazdak <nazemi.works@gmail.com>
2019-07-17 00:42:39 -0700

    stripe back end

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 if(process.env.Node_env !== 'production') 
   require('dotenv').config();
+// give us back stripe obj
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +28,23 @@ if (process.env.Node_env === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });  
 }
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+})
+
+
 
 app.listen(port, error => {
   if (error) throw error;
