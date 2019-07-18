3fb87cf81898485b30520d5f58bff859578022c6
mazdak <nazemi.works@gmail.com>
2019-07-18 00:18:09 -0700

    use compression package on server side code to implement gizp

--- client/src/components/CartItem/CartItem.js
+++ client/src/components/CartItem/CartItem.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './CartItemStyles.scss';
+// useCallback let us memoize inline function on func comps
+// const sth = useCallback(() => cl(sth), [arr of dependencies]);
 
+// use Memo => to cache value of comp that does sth complex like match
+// for not re compute useMemo(()=> {}, [])
 const CartItem = ({ item : {imageUrl, price, name, quantity }}) => {
   return (
     <div className='cart-item'>
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+// must add gzip to make our chunk and bundle files small on servers like heroku
+// otherwise gzip of react does not work
+// use npm compression package
+const compression = require('compression');
 
 if(process.env.Node_env !== 'production') 
   require('dotenv').config();
@@ -12,6 +15,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 // any process come in make sure body and tag convert to json
 app.use(bodyParser.json());
 // make sure url in or out does not contain space symbols otherwise escape it
